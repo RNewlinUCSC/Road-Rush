964eab61cbcb643c6be7b48bf34c0d1a037d4792
Robert Newlin <rnewlin@ucsc.edu>
2022-04-20 13:49:08 -0700

    Should create a cube, but currently broke the game

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,4 +2,13 @@ class Play extends Phaser.Scene {
     constructor() {
         super("Play");
     }
+
+    preload() {
+        //load images here
+        this.preload.image('playerCube', './assets/cubePink.png');
+    }
+
+    create() {
+        this.player = new Cube(this, this.game.config.width/2, this.game.config.height/2, 0, 'playerCube').setOrigin(0,0);
+    }
 }
